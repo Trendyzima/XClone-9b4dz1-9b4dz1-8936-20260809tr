1133ade4f68b9b772a3ff26a65cdf9b3b6f14755
Trendyzima <momanyitrendy@gmail.com>
2026-09-19 16:50:16 +0300

    perf(heartbeat): extend edge suppression to 30 minutes

--- cloudflare/heartbeat-worker/src/index.ts
+++ cloudflare/heartbeat-worker/src/index.ts
@@ -3,7 +3,7 @@ interface Env {
   SUPABASE_PUBLISHABLE_KEY: string;
 }
 
-const SUPPRESSION_SECONDS = 14 * 60 + 55;
+const SUPPRESSION_SECONDS = 29 * 60 + 55;
 
 const ALLOWED_ORIGINS = new Set([
   "https://testagram.site",
