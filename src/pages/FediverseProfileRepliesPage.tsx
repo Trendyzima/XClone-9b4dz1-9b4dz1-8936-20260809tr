c3b6667d433e7cd14ec9c475912b25aa3aa7eb6b
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 15:03:01 +0300

    refactor(fediverse): add standalone remote profile replies route

---
+++ src/pages/FediverseProfileRepliesPage.tsx
@@ -0,0 +1,2 @@
+import FediverseProfilePage from '@/pages/FediverseProfilePage';
+export default function FediverseProfileRepliesPage() { return <FediverseProfilePage initialTab="Replies" standalone />; }
