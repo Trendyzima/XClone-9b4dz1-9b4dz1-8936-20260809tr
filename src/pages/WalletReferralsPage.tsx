1a705b16775d08694f23b0d2c126d072a6894db8
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 07:41:58 +0300

    refactor(wallet): add standalone referrals page

---
+++ src/pages/WalletReferralsPage.tsx
@@ -0,0 +1,6 @@
+import WalletPage from '@/pages/WalletPage';
+
+/** Standalone wallet surface: Wallet referrals. */
+export default function WalletReferralsPage() {
+  return <WalletPage initialTab="referrals" standaloneTitle="Wallet referrals" />;
+}
