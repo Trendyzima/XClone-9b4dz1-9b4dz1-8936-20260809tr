7dcf13991cfb7d8bb0e50d02f162c133f1b32b66
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 07:41:49 +0300

    refactor(wallet): add standalone history page

---
+++ src/pages/WalletHistoryPage.tsx
@@ -0,0 +1,6 @@
+import WalletPage from '@/pages/WalletPage';
+
+/** Standalone wallet surface: Wallet history. */
+export default function WalletHistoryPage() {
+  return <WalletPage initialTab="history" standaloneTitle="Wallet history" />;
+}
