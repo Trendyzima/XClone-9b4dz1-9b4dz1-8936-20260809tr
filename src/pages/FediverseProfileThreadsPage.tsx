f9156142cadebeda037544641562fff54b868271
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 15:02:58 +0300

    refactor(fediverse): add standalone remote profile threads route

---
+++ src/pages/FediverseProfileThreadsPage.tsx
@@ -0,0 +1,2 @@
+import FediverseProfilePage from '@/pages/FediverseProfilePage';
+export default function FediverseProfileThreadsPage() { return <FediverseProfilePage initialTab="Threads" standalone />; }
