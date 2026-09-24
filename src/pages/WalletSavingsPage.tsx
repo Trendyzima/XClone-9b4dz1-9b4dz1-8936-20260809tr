73e07b5b40dcac5723341510fdd35c8c981bd85b
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 07:42:03 +0300

    refactor(wallet): add standalone savings page

---
+++ src/pages/WalletSavingsPage.tsx
@@ -0,0 +1,6 @@
+import WalletPage from '@/pages/WalletPage';
+
+/** Standalone wallet surface: Savings goals. */
+export default function WalletSavingsPage() {
+  return <WalletPage initialTab="savings" standaloneTitle="Savings goals" />;
+}
