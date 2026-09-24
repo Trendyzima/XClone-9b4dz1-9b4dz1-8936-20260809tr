766bc52903105cfb2a9dbb2f2a6e0afdeec228da
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 07:41:52 +0300

    refactor(wallet): add standalone send page

---
+++ src/pages/WalletSendPage.tsx
@@ -0,0 +1,6 @@
+import WalletPage from '@/pages/WalletPage';
+
+/** Standalone wallet surface: Send money. */
+export default function WalletSendPage() {
+  return <WalletPage initialTab="send" standaloneTitle="Send money" />;
+}
