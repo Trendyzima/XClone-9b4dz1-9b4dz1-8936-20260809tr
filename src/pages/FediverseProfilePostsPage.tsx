5231c6f7dbcd769fff8cd3c275113e9d3c41ea59
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 15:02:55 +0300

    refactor(fediverse): add standalone remote profile posts route

---
+++ src/pages/FediverseProfilePostsPage.tsx
@@ -0,0 +1,2 @@
+import FediverseProfilePage from '@/pages/FediverseProfilePage';
+export default function FediverseProfilePostsPage() { return <FediverseProfilePage initialTab="Posts" standalone />; }
