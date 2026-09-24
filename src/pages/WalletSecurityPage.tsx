d4f35e04c281e7bf9873695b1c08ad5aa6c8eaa9
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 07:42:07 +0300

    refactor(wallet): add standalone security page

---
+++ src/pages/WalletSecurityPage.tsx
@@ -0,0 +1,6 @@
+import WalletPage from '@/pages/WalletPage';
+
+/** Standalone wallet surface: Wallet security. */
+export default function WalletSecurityPage() {
+  return <WalletPage initialTab="security" standaloneTitle="Wallet security" />;
+}
