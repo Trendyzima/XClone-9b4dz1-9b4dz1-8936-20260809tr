ed43267ef957461bcfb600d85ff78d2d1198a123
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 07:42:15 +0300

    refactor(wallet): add standalone mpesa page

---
+++ src/pages/WalletMpesaPage.tsx
@@ -0,0 +1,6 @@
+import WalletPage from '@/pages/WalletPage';
+
+/** Standalone wallet surface: M-Pesa wallet. */
+export default function WalletMpesaPage() {
+  return <WalletPage initialTab="mpesa" standaloneTitle="M-Pesa wallet" />;
+}
