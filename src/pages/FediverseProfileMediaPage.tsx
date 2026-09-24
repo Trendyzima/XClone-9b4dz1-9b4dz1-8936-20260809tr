1df431954c60fe1b73eea2c17191f8ae06408076
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 15:03:03 +0300

    refactor(fediverse): add standalone remote profile media route

---
+++ src/pages/FediverseProfileMediaPage.tsx
@@ -0,0 +1,2 @@
+import FediverseProfilePage from '@/pages/FediverseProfilePage';
+export default function FediverseProfileMediaPage() { return <FediverseProfilePage initialTab="Media" standalone />; }
