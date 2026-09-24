7801a443b81001ccd4313e1f56817d537aad9dd6
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 07:42:09 +0300

    refactor(wallet): add standalone converter page

---
+++ src/pages/WalletConverterPage.tsx
@@ -0,0 +1,6 @@
+import WalletPage from '@/pages/WalletPage';
+
+/** Standalone wallet surface: Currency converter. */
+export default function WalletConverterPage() {
+  return <WalletPage initialTab="converter" standaloneTitle="Currency converter" />;
+}
