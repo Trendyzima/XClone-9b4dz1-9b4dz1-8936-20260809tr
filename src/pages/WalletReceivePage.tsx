72e34b2ba166888dc69670f842030664234de9d1
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 07:41:54 +0300

    refactor(wallet): add standalone receive page

---
+++ src/pages/WalletReceivePage.tsx
@@ -0,0 +1,6 @@
+import WalletPage from '@/pages/WalletPage';
+
+/** Standalone wallet surface: Receive money. */
+export default function WalletReceivePage() {
+  return <WalletPage initialTab="receive" standaloneTitle="Receive money" />;
+}
