5ff07a060207a007c0fd479163ec5477c129e6d1
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 07:42:12 +0300

    refactor(wallet): add standalone pocket page

---
+++ src/pages/WalletPocketPage.tsx
@@ -0,0 +1,6 @@
+import WalletPage from '@/pages/WalletPage';
+
+/** Standalone wallet surface: Savings pocket. */
+export default function WalletPocketPage() {
+  return <WalletPage initialTab="pocket" standaloneTitle="Savings pocket" />;
+}
