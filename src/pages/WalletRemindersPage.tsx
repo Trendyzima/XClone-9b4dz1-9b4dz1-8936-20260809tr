8f6dfb29e6b884d5b15d689eb76834fe21304d1b
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 07:42:05 +0300

    refactor(wallet): add standalone reminders page

---
+++ src/pages/WalletRemindersPage.tsx
@@ -0,0 +1,6 @@
+import WalletPage from '@/pages/WalletPage';
+
+/** Standalone wallet surface: Transaction reminders. */
+export default function WalletRemindersPage() {
+  return <WalletPage initialTab="reminders" standaloneTitle="Transaction reminders" />;
+}
