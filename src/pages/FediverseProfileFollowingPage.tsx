b548bf4baced5958946c5392db9fe7dd0175be9f
Trendyzima <momanyitrendy@gmail.com>
2026-09-24 15:03:23 +0300

    refactor(fediverse): add standalone remote profile following route

---
+++ src/pages/FediverseProfileFollowingPage.tsx
@@ -0,0 +1,2 @@
+import FediverseProfilePage from '@/pages/FediverseProfilePage';
+export default function FediverseProfileFollowingPage() { return <FediverseProfilePage initialTab="Following" standalone />; }
