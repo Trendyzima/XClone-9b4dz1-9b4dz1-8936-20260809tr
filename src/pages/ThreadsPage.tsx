df9ee1b5c9114f38afcbd9f216ac7ec2cbcc8309
Trendyzima <momanyitrendy@gmail.com>
2026-09-25 14:27:14 +0300

    fix(threads): restore federated hashtag discovery import on main

--- src/pages/ThreadsPage.tsx
+++ src/pages/ThreadsPage.tsx
@@ -15,6 +15,7 @@ import { FeedAdCard } from '@/components/features/FeedAdCard';
 import { DynamicAd } from '@/components/features/DynamicAd';
 import * as federation from '@/api/federation';
 import { FederatedHashtagDiscovery } from '@/components/features/FederatedOrganicDiscovery';
+import { FederatedHashtagDiscovery } from '@/components/features/FederatedOrganicDiscovery';
 import { toggleThreadLike, getThreadLikeStates } from '@/features/threadLikes/threadLikesService';
 import { toggleThreadRepost, getThreadRepostStates } from '@/features/threadReposts/threadRepostsService';
 
